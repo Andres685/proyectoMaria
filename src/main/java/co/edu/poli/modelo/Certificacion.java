2c09a3c81e3c11a610e9a585879724c14937c881
Andres Guzman <guzmanandres685@gmail.com>
2025-03-20 12:28:55 -0500

    Formulario con 3 Patrones

--- src/main/java/co/edu/poli/modelo/Certificacion.java
+++
@@ -1,8 +0,0 @@
-package co.edu.poli.modelo;
-
-public class Certificacion {
-    private static final String TIPO = "Certificacion de Alta Calidad";
-    public String getTipo() {
-        return TIPO;
-    }
-}
--- src/main/java/co/edu/poli/modelo/Evaluacion.java
+++
@@ -1,9 +0,0 @@
-package co.edu.poli.modelo;
-
-public class Evaluacion {
-    private static final String EVALUACION = "5 estorellas";
-
-    public String getEvaluacion() {
-        return EVALUACION;
-    }
-}
