2df782fdeed8afcfde844984db6dc6990bbbc4cf
Andres Guzman <guzmanandres685gmail.com>
2025-04-09 23:38:27 -0500

    Actividad 9 Funcionando en Eclipse

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,13 +4,17 @@ module co.edu.poli {
     requires javafx.graphics;
     requires javafx.base;
     requires java.sql;
-	requires java.desktop;
+    requires java.desktop;
 
     opens co.edu.poli.controlador to javafx.fxml;
-    opens co.edu.poli.modelo to javafx.base;
+    opens co.edu.poli.modelo.PatronFacade to javafx.base;
+    opens co.edu.poli.modelo.PatronFlyWeight to javafx.base;
+    opens co.edu.poli.modelo.PatronProxy to javafx.base;
     opens co.edu.poli.vista to javafx.fxml;
 
     exports co.edu.poli.controlador;
-    exports co.edu.poli.modelo;
+    exports co.edu.poli.modelo.PatronFacade;
+    exports co.edu.poli.modelo.PatronFlyWeight;
+    exports co.edu.poli.modelo.PatronProxy;
     exports co.edu.poli.vista;
 }
